e54bf50315eb9e6cc99beb7fc7d5b65e30565168
baum123 <116869422+lunatics975@users.noreply.github.com>
2023-07-24 12:42:24 +0200

    fixes
    interpunktion

--- Nixenrechner.cpp
+++ Nixenrechner.cpp
@@ -5,8 +5,8 @@
 //dann anzahl der nixe/euro
 #include <iostream>
 using namespace std;
-int euro = 0;
-int nixe = 0;
+long euro = 0;
+long nixe = 0;
 int choose = 0;
 int main() {
 cout<<"Willst du Euro in Nixe (1)?"<<endl;
@@ -14,17 +14,19 @@ cout<<"oder"<<endl;
 cout<<"Nixe in Euro (2)?"<<endl;
 cin >> choose;
 if (choose == 1){
-    cout<<"Gebe die Euroanzahl an"<<endl;
+    cout<<"Gebe die Euroanzahl an:"<<endl;
     cin >> euro;
     nixe = euro*42;
-    cout<<"Das sind "<< nixe << "Nixe"<<endl;
+    cout<<"Das sind "<< nixe << " Nixe."<<endl;
+    cout<<"Zum erneutem Rechnen Programm neu starten."<<endl;
 }
 
-if (choose == 0){
-    cout<<"Gebe die Nixeanzahl an" <<endl;
+if (choose == 2){
+    cout<<"Gebe die Nixeanzahl an:" <<endl;
     cin >> nixe;
     euro=nixe/42;
-    cout << "Das sind " << euro << "Euro"<<endl;
+    cout << "Das sind " << euro << " Euro."<<endl;
+    cout<<"Zum erneutem Rechnen Programm neu starten."<<endl;
 }
 	return 0;
 }
