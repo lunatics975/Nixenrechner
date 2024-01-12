d44adca42d96b696814f3f05d7b892afd6c1a122
baum123 <116869422+lunatics975@users.noreply.github.com>
2024-01-12 23:04:06 +0000

    optimzed code

--- Nixenrechner.cpp
+++ Nixenrechner.cpp
@@ -1,42 +1,27 @@
-//dies ist ein nixenrechner von euro in nixe und andersherum
-//bitte gebe die zahlen unereinabder ein, damit es funktioniert
-//also 1: euro -> nixe
-//also 2: nixe -> euro
-//dann anzahl der nixe/euro
 #include <iostream>
 using namespace std;
-long euro = 0;
-long nixe = 0;
-int choose = 0;
-int main() {
-cout<<"Willst du Euro in Nixe (1)?"<<endl;
-cout<<"oder"<<endl;
-cout<<"Nixe in Euro (2)?"<<endl;
-cin >> choose;
-if (choose == 1){
-    cout<<"Gebe die Euroanzahl an:"<<endl;
-    cin >> euro;
-    if (euro<0){
-        cout<<"Die angegebene Zahl ist negativ. Bitte versuch es erneut und gib eine positive Euroanzahl an."<<endl;
-    }else{
-        nixe = euro*42;
-        cout<<"Das sind "<< nixe << " Nixe."<<endl;
-        cout<<"Zum erneutem Rechnen Programm neu starten."<<endl;
-    }
-}
 
-else if (choose == 2){
-    cout<<"Gebe die Nixeanzahl an:" <<endl;
-    cin >> nixe;
-    if (nixe<0){
-            cout<<"Die angegebene Zahl ist negativ. Bitte versuch es erneut und gib eine positive Euroanzahl an."<<endl;        
-    }else{
-        euro=nixe/42;
-        cout << "Das sind " << euro << " Euro."<<endl;
-        cout<<"Zum erneutem Rechnen Programm neu starten."<<endl;
+int main() {
+    int choose = 0;
+    long euro = 0, nixe = 0;
+    cout << "Willkommen beim Nixenrechner.";
+
+    cout << "Moechtest du Euro in Nixe (1) oder Nixe in Euro (2) umrechnen?" << endl;
+    cin >> choose;
+
+    if (choose == 1 || choose == 2) {
+        cout << "Gebe die " << (choose == 1 ? "Euro" : "Nixe") << "-Anzahl an:" << endl;
+        cin >> ((choose == 1) ? euro : nixe);
+
+        if ((choose == 1 && euro >= 0) || (choose == 2 && nixe >= 0)) {
+            cout << "Das sind " << ((choose == 1) ? euro * 42 : nixe / 42) << " " << ((choose == 1) ? "Nixe" : "Euro") << "." << endl;
+            cout << "Zum erneuten Rechnen Programm neu starten." << endl;
+        } else {
+            cout << "Die angegebene Zahl ist negativ. Bitte versuche es erneut und gib eine positive " << ((choose == 1) ? "Euro" : "Nixe") << "-Anzahl an." << endl;
+        }
+    } else {
+        cout << "Ungueltige Angabe. Bitte erneut versuchen." << endl;
     }
-}else{
-    cout<<"Unguelitige Angabe. Bitte erneut versuchen."<<endl;
-}
-	return 0;
+
+    return 0;
 }
